d0d5dbe7ce6a2d9cc9c496cef56b9baeb684d481
Omnarkar31 <narkarom161@gmail.com>
2025-11-01 19:32:25 +0530

    Add files via upload

---
+++ TapNpay.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tapNpay;
+
+/**
+ *
+ * @author Ani
+ */
+public class TapNpay {
+
+    public static void main(String[] args) {
+       new Login().setVisible(true);
+    }
+}
